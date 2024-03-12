9792d38b11238412b24e19db6d2e54be07a25571
Victor Le Roux <vle-roux@c1r2p4.42lehavre.fr>
2024-03-12 18:59:33 +0100

    comprehension de la MLX42 approfondie

--- main.c
+++ main.c
@@ -10,48 +10,23 @@
 
 #define WIDTH 512
 #define HEIGHT 512
-
-static mlx_image_t* image;
-
-// -----------------------------------------------------------------------------
-
-int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
-{
-    return (r << 24 | g << 16 | b << 8 | a);
-}
-
-void ft_randomize(void* param)
-{
-	(void)param;
-	for (uint32_t i = 0; i < image->width; ++i)
-	{
-		for (uint32_t y = 0; y < image->height; ++y)
-		{
-			uint32_t color = ft_pixel(
-				rand() % 0xFF, // R
-				rand() % 0xFF, // G
-				rand() % 0xFF, // B
-				rand() % 0xFF  // A
-			);
-			mlx_put_pixel(image, i, y, color);
-		}
-	}
-}
+#define WIDTH_HERO 104
+#define HEIGHT_HERO 232
+static mlx_image_t* hero;
 
 void ft_hook(void* param)
 {
 	mlx_t* mlx = param;
-
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
-	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-		image->instances[0].y -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-		image->instances[0].y += 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		image->instances[0].x -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		image->instances[0].x += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_W)&& hero->instances[0].y > 0)
+		hero->instances[0].y -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_S)&& hero->instances[0].y < HEIGHT - HEIGHT_HERO)
+		hero->instances[0].y += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_A)&& hero->instances[0].x > 0 )
+		hero->instances[0].x -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_D)&& hero->instances[0].x < WIDTH - WIDTH_HERO) 
+		hero->instances[0].x += 5;
 }
 
 // -----------------------------------------------------------------------------
@@ -59,29 +34,25 @@ void ft_hook(void* param)
 int32_t main(void)
 {
 	mlx_t* mlx;
-
+	mlx_texture_t* img_hero;
+	img_hero = mlx_load_png("./sprite/hero_idle.png");
 	// Gotta error check this stuff
 	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
 	{
 		puts(mlx_strerror(mlx_errno));
 		return(EXIT_FAILURE);
 	}
-	if (!(image = mlx_new_image(mlx, 128, 128)))
-	{
-		mlx_close_window(mlx);
-		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
-	}
-	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
-	{
-		mlx_close_window(mlx);
-		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
-	}
 	
-	mlx_loop_hook(mlx, ft_randomize, mlx);
-	mlx_loop_hook(mlx, ft_hook, mlx);
-
+	
+	hero = mlx_texture_to_image(mlx,img_hero);
+	if(!hero)
+	{	
+		mlx_delete_texture(img_hero);
+		mlx_terminate(mlx);
+		return EXIT_FAILURE;
+	}
+	mlx_image_to_window(mlx,hero,0,0);
+	mlx_loop_hook(mlx,ft_hook,mlx);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
